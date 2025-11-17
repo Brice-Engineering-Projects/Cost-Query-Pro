5818c793bedbf6b7e6ab07763368ffd639f11d9d
Brice Nelson <bnelsonemail@icloud.com>
2025-11-17 14:12:05 -0500

    tests: Ran black to reformat

--- tests/unit_tests/test_auth_jwt.py
+++ tests/unit_tests/test_auth_jwt.py
@@ -74,7 +74,9 @@ def test_jwt_token_created_on_login(create_test_user):
     assert "access_token" in data
     token = data["access_token"]
 
-    _decoded = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm]) # noqa: F841
+    _decoded = jwt.decode(
+        token, settings.secret_key, algorithms=[settings.algorithm]
+    )  # noqa: F841
     assert _decoded.get("sub") == create_test_user["username"]
     assert "exp" in _decoded
 
